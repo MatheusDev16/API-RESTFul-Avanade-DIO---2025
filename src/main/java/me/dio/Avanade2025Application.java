62364ad9dbffcf7f8824e9011be393652150af6f
Matheus Almeida <Matheusmonfoy@hotmail.com>
2025-03-21 16:11:16 -0300

    Bootcamp Avanade DIO
    API RESTful para o Bootcamp Avanade Decola Tech 2025 da DIO

---
+++ src/main/java/me/dio/Avanade2025Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Avanade2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Avanade2025Application.class, args);
+	}
+
+}
